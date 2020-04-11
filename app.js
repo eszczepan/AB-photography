38152c1ed7a581df93da9841b2c876f83e944264
Szczepan <s.szablej1@gmail.com>
2020-04-11 22:05:03 +0200

    Add contact section + footer

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-links");
+const links = nav.querySelectorAll("a");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-links-active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-links-active");
+  });
 });
